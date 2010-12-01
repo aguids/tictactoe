a99222ac8c56b6904cd2402a8989bebfe8397f2f
Felipe Doria <felipe.doria@gmail.com>
2010-12-01 03:57:23 -0300

    Properly initialize the board state when creating a new instance of the game.

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,26 +1,27 @@
 class TicTacToe
   
+  def initialize
+    @board = [[nil,nil,nil],
+              [nil,nil,nil],
+              [nil,nil,nil]]
+  end
+  
   def play
-    board   = [[nil,nil,nil],
-               [nil,nil,nil],
-               [nil,nil,nil]]
-
     left_diagonal = [[0,0],[1,1],[2,2]]
     right_diagonal = [[2,0],[1,1],[0,2]]
-
 
     players = [:X, :O].cycle
 
     current_player = players.next 
 
     loop do
-      puts board.map { |row| row.map { |e| e || " " }.join("|") }.join("\n")
+      puts @board.map { |row| row.map { |e| e || " " }.join("|") }.join("\n")
       print "\n>> "
       row, col = gets.split.map { |e| e.to_i }
       puts
 
       begin
-        cell_contents = board.fetch(row).fetch(col)
+        cell_contents = @board.fetch(row).fetch(col)
       rescue IndexError
         puts "Out of bounds, try another position"
         next
@@ -31,7 +32,7 @@ class TicTacToe
         next
       end
 
-      board[row][col] = current_player
+      @board[row][col] = current_player
 
       lines = []
 
@@ -43,7 +44,7 @@ class TicTacToe
       lines << (0..2).map { |r1| [r1, col] }
 
       win = lines.any? do |line|
-        line.all? { |row,col| board[row][col] == current_player }
+        line.all? { |row,col| @board[row][col] == current_player }
       end
 
       if win
@@ -51,7 +52,7 @@ class TicTacToe
         exit
       end
 
-      if board.flatten.compact.length == 9
+      if @board.flatten.compact.length == 9
         puts "It's a draw!"
         exit
       end
